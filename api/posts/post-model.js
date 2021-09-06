75bb9042fd8d048fd0b5097edcbc3d367b7c7ebf
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-06 16:22:14 -0400

    ...

--- api/posts/post-model.js
+++ api/posts/post-model.js
@@ -8,24 +8,33 @@ module.exports = {
   remove,
 };
 
-async function get() {
+function get() {
   //select * from posts
   return db('posts'); // db == knex in docs.
 }
 
-async function getById(id) {
+function getById(id) {
   // return db('posts').where({id: id, foo: 'bar'}).first(); // without .first() We always get and [array] which could be empty []
   return db('posts').where('id', id).first(); //simpler returns a collection// .first will chain the first one
 }
 
-async function create() {
-  return 'create wired';
+async function create({ title, contents }) {
+  const [id] = await db('posts').insert({ title, contents }); //[#] array of the id of new record
+  //console.log(id);//if you dont know what it is console.log(stuff) it first
+  const newPost = await getById(id);
+  // console.log(newPost);
+  return newPost;
 }
 
-async function update() {
-  return 'update wired';
+async function update(id, { title, contents }) {
+  await db('posts').where('id', id).update({ title, contents });
+  return getById(id);
 }
 
-async function remove() {
-  return 'delete wired';
+async function remove(id) { //README will tell what to be returned.
+  // what does it return? returns # of things deleted
+  //what to do to give back the deleted thing??
+  const deletedPost = await getById(id);
+  await db('posts').where('id', id).del();
+  return deletedPost;
 }
