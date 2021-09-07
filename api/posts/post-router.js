bb1fcaf72eb57b76c7c24c4126e4b90040006ad1
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-07 13:36:42 -0400

    ..

--- api/posts/post-router.js
+++ api/posts/post-router.js
@@ -1,66 +1,82 @@
-const express = require('express')
-const Post = require('./post-model')
+const express = require('express');
+const Post = require('./post-model');
 
-const router = express.Router()
+const router = express.Router();
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+  try {
+    const post = await Post.getById(req.params.id);
+    if (post) {
+      req.post = post;
+      next();
+    }
+    else {
+      res.status(404).json({ message: "post not found" });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 function checkPayload(req, res, next) {
-  next()
+  const { title, contents } = req.body;
+  if (title && contents) {
+    next();
+  } else {
+    res.status(400).json({ message: "title and contents required" });
+  }
 }
 
 router.get('/', async (req, res, next) => {
   try {
-    const data = await Post.get()
-    res.json(data)
+    const data = await Post.get();
+    res.json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.get('/:id', checkId, async (req, res, next) => {
   try {
-    const data = await Post.getById(req.params.id)
-    res.json(data)
+    const data = await Post.getById(req.params.id);
+    res.json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.post('/', checkPayload, async (req, res, next) => {
   try {
-    const data = await Post.create(req.body)
-    res.json(data)
+    const data = await Post.create(req.body);
+    res.json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.put('/:id', checkPayload, checkId, async (req, res, next) => {
   try {
-    const data = await Post.update(req.params.id, req.body)
-    res.json(data)
+    const data = await Post.update(req.params.id, req.body);
+    res.json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.delete('/:id', checkId, async (req, res, next) => {
   try {
-    const data = await Post.remove(req.params.id)
-    res.json(data)
+    const data = await Post.remove(req.params.id);
+    res.json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
